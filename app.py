5b4d60a48d19f33fde80797f010d88e11c926432
Luka Hietala <luka.hietala08@gmail.com>
2025-11-18 18:57:10 +0200

    list bare repos

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 
+from git.repo import get_bare_repos
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html")
+def index():
+    repos = get_bare_repos("/home/lhietala/git-webview/repos-example")
+    return render_template("index.html", repos=repos)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ git/repo.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+import pygit2 as git
+
+# scans given path for bare git repos and list their names and paths
+def get_bare_repos(path):
+    repos = []
+
+    repo_path = Path(path)
+    for item in repo_path.iterdir():
+        if item.is_dir():
+            try:
+                repo = git.Repository(str(item))
+                if repo.is_bare:
+                    repo_info = {
+                        "name": item.name,
+                        "path": str(item.resolve())
+                    }
+                    repos.append(repo_info)
+            except git.GitError:
+                continue
+    return repos
