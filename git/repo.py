ad8641ac4b4914c58310bb6bf8dad5ac26319a56
Luka Hietala <luka.hietala08@gmail.com>
2025-11-26 19:38:03 +0200

    view repo owner

--- git/repo.py
+++ git/repo.py
@@ -1,9 +1,13 @@
 from pathlib import Path
 import pygit2 as git
+import os
+from .db import init_db, get_owner
 
 # scans given path for bare git repos and list their names and paths
 def get_bare_repos(path):
     repos = []
+    db_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'lipasto.db')
+    init_db(db_path)
 
     repo_path = Path(path)
 
@@ -19,10 +23,12 @@ def get_bare_repos(path):
                             description = desc_file.read_text().strip()
                         except:
                             pass
+                    owner = get_owner(db_path, item.name)
                     repo_info = {
                         "name": item.name,
                         "path": str(item.resolve()),
-                        "description": description
+                        "description": description,
+                        "owner": owner
                     }
                     repos.append(repo_info)
             except git.GitError:
